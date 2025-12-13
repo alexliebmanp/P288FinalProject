f18593ec844747325623bf31ae72dd6d2a84c6e9
Alex Liebman-Pelaez <alexliebmanp@gmail.com>
2025-12-13 13:45:18 -0800

    added first working version of AVERT LSTM

---
+++ lstm.py
@@ -0,0 +1,272 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from keras import optimizers
+from keras.layers import LSTM
+from keras.layers import Dense, Reshape
+from keras.models import Sequential
+import math
+
+class AVERT_LSTM():
+    """
+    LSTM class for many-to-many forecasting of volcanic activity. Currently predicts all variables.
+    """
+
+    def __init__(self, df, lstm_vars =['Eruption_Activity', 'VPCC_RSAM', 'VPPC_RSAM', 'VPNC_RSAM', 'VPRS_RSAM', 'VPRS_LFE',
+       'VPRS_LFN', 'VPRS_LFZ', 'VPPC_LFE', 'VPPC_LFN', 'VPPC_LFZ', 'VPNC_LFE',
+       'VPNC_LFN', 'VPNC_LFZ', 'CO2_ppm', 'lake_size']):
+        """
+
+        Load data and preprocess by normalizing and scaling.
+
+        args:
+            - df:           (DataFrame) contains time-series data with Datetime index
+            - lstm_vars:    (list) names of columns in df to use in LSTM
+        """
+
+        # filter data to columns we want in LSTM and handle any nans
+        df = df[lstm_vars]
+        df.fillna(0, inplace=True)
+        self.df = df # save DataFrame
+        
+        # preprocess data into numpy arrays
+        self.df_scaled, self.scaler = self.NormAndScale(df)
+        self.data = self.df_scaled.to_numpy()
+        self.index = self.df.index
+
+    def CreateModel(self, n_past=1, n_future=1, n_divide=0.8, n_neurons=400, n_epochs=150, learning_rate=0.001, momentum=0.4, opt ='adam', activation='tanh'):
+        """
+
+        Creates many-to-many LSTM model by reframing time-series data as supervised learning X and Y data with shapes
+
+        X.shape = (n_times, n_past, n_features)
+        Y.shape = (n_times, n_future, n_features)
+
+        where n_features is the number of columns in df. It then defines a model with the following architecture
+
+        LSTM -> Dense Layer -> Reshape Layer
+
+        such that input is of shape (n_past, n_features) and output is of shape (n_future, n_features). Could be extended in the future to select how many features to actually predict on.
+
+        While the function has no output, it stores the model as a class variable, as well as the test/train X and Y data and model arguments.
+
+        args:
+            - n_past:           number of time points to use as input to LSTM
+            - n_future:         number of time points to predict out of the Dense Layer
+            - n_divide:         train/test split fraction
+            - n_neurons:        number of neurons in LSTM layer
+            - n_epochs:         number of epochs for training
+            - learning_rate:    learning rate used in gradient descent
+            - momentum:         momentum used in gradient descent
+            - opt:              optimization routine
+            - activation:       activation function in LSTM layer. No activation is implemented in Dense layer.
+        """
+        
+        #calling normalized and scaled data
+        data = self.data
+        n_features = data.shape[1]
+        self.n_features = n_features
+        
+        # reframe as learning problem
+        X, Y = self.PrepareData(data, n_past, n_future)
+        n_divide = round(len(data[:,0])*n_divide)
+        train_X, train_Y = X[:n_divide], Y[:n_divide]
+        test_X, test_Y = X[n_divide:], Y[n_divide:]
+
+        # keep track of Datetime index across train/test set
+        self.train_index = self.index[:n_divide]
+        self.test_index = self.index[n_divide:]
+ 
+        #design network: n_neurons stands for size of hidden layer
+        model = Sequential()
+        model.add(LSTM(n_neurons, input_shape=(n_past, n_features), activation=activation))
+        model.add(Dense(n_future * n_features))
+        model.add(Reshape((n_future, n_features)))
+        if opt == 'sgd':
+            Opt = optimizers.SGD(learning_rate = learning_rate,\
+                                 momentum = momentum)
+        if opt == 'adam':
+            Opt = optimizers.Adam(learning_rate = learning_rate)
+        model.compile(loss = 'mae', optimizer = Opt)
+
+        # store
+        self.opt = opt
+        self.momentum = momentum
+        self.learning_rate = learning_rate
+        self.n_epochs = n_epochs
+        self.n_past   = n_past
+        self.n_future = n_future
+        self.model    = model
+        self.test_X   = test_X
+        self.test_Y   = test_Y
+        self.train_X  = train_X
+        self.train_Y  = train_Y
+        self.n_divide = n_divide
+
+    def Fit(self):
+        """
+        Fits self.model and plots loss history over train and test sets.
+        """
+
+        model = self.model
+        n_epochs = self.n_epochs
+        train_X = self.train_X
+        train_Y = self.train_Y
+        test_X = self.test_X
+        test_Y = self.test_Y
+
+        # fit network
+        print('Performing Training...')
+        history = model.fit(train_X, train_Y, epochs=n_epochs, batch_size =24*14, validation_data=(test_X, test_Y), verbose=2, shuffle=False)
+        print('...Training Done!')
+
+        #plot history
+        plt.plot(history.history['loss'], label='train')
+        plt.plot(history.history['val_loss'], label='test')
+        plt.xlabel('epoch')
+        plt.ylabel('loss [MSE]')
+        plt.legend()
+        plt.show()
+
+    def Predict(self):
+        """
+        predit Yhat on test_X, unscale, store in a DataFrame self.df_pred. Currently just takes the predictions from max n_future.
+        """
+
+        model = self.model
+        X = self.test_X
+        Yhat = model.predict(X)
+        back = Yhat.shape[0]
+        Yhat_last = Yhat[:,-1,:] # keep just the last time point predicted for each time
+        index = self.df.index[-back:]
+        columns = self.df.columns
+        df_scaled = pd.DataFrame(Yhat_last, columns=columns, index=index) # not sure yet how to handle index
+        df = self.invNormAndScale(df_scaled, self.scaler)
+        self.df_pred = df
+
+    def Forecast(self):
+        """
+        Macro that predicts forecast on test_X and then plots.
+        """
+        
+        self.Predict()
+        self.PlotData([self.df, self.df_pred])
+
+        # plot these two things together using PlotData to see how the prediction fared  
+
+    ### Helper Functions ###
+
+    def PlotData(self, dfs=None, log_vars=['Eruption_Activity', 'VPCC_RSAM', 'VPPC_RSAM', 'VPNC_RSAM', 'VPRS_RSAM']):
+        """
+        Plots data for each DataFrame in dfs:
+
+        args:
+            - dfs:              (list) DataFrame objects to plot. Each DataFrame must have matching columns
+            - log_vars:         (list) names of columns to plot in log scale
+        """
+
+        if dfs is None:
+            s = [self.df]
+        if isinstance(dfs, pd.DataFrame):
+            dfs = [dfs]
+        columns = dfs[0].columns
+        scale = 3
+        fig, ax = plt.subplots(math.ceil(len(columns)/2), 2, figsize=(8*scale, 0.75*scale*len(columns)), constrained_layout=True)
+        ax = ax.flatten()
+
+        for df in dfs:
+            for ii, c in enumerate(columns):
+                df[c].plot(ax=ax[ii])
+                ax[ii].set(ylabel=c, xlabel='Time')
+                if c in log_vars:
+                    ax[ii].set_yscale('log')
+                
+                # Format x-axis for proper DateTime display
+                ax[ii].xaxis.set_major_locator(plt.MaxNLocator(nbins=6))  # Limit number of ticks
+                ax[ii].tick_params(axis='x', rotation=45)  # Rotate labels for readability
+                
+                # If the index is datetime, use date formatting
+                if pd.api.types.is_datetime64_any_dtype(df.index):
+                    import matplotlib.dates as mdates
+                    ax[ii].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+                    ax[ii].xaxis.set_major_locator(mdates.AutoDateLocator())
+
+        fig.tight_layout(pad=10)
+        plt.show()
+
+    def PrepareData(self, data, n_past, n_future):
+        """
+        Reframes time-series data as learning X and Y data.
+
+            data.shape = (n_times, n_features)
+            x.shape = (n_times*, n_past, n_features)
+            y.shape = (n_times*, n_future, n_features)
+
+        where n_times* = n_times - n_past - n_future + 1
+
+        args:
+            - data:             (ndarray)          
+            - n_past:           (int)
+            - n_future:         (int)
+
+        return:
+            - X:                (ndarray)
+            - Y:                (ndarray)
+        """
+        X, Y = [], []
+        for i in range(n_past, len(data) - n_future + 1):
+            X.append(data[i - n_past:i])
+            Y.append(data[i:i + n_future])
+        return np.array(X), np.array(Y)
+    
+    def NormAndScale(self, df):
+        """
+        Takes DataFrame as input and returns another DataFrame with same shape with scaled data from [0,1] using MinMaxScaler
+
+        args:
+            - df:         (DataFrame) data to scale
+
+        returns:
+            - df_scaled   (DataFrame) scaled data
+            - scaler:     (MinMaxScaler) scaler transform object
+        """
+        
+        # extracting values from dataframe
+        df_scaled = df.copy()
+        data = df_scaled.to_numpy()
+        
+        # normalize
+        scaler = MinMaxScaler(feature_range=(0, 1)) #normalize features
+        scaled = scaler.fit_transform(data)
+        
+        # storing scaled values into a dataframe
+        df_scaled[:] = scaled
+            
+        return df_scaled, scaler
+
+    def invNormAndScale(self, df_scaled, scaler):
+        """
+        Inverse of NormAndScale. Takes as input scaled data and scaler and return unscaled data
+
+        args:
+            - df_scaled   (DataFrame) scaled data
+            - scaler:     (MinMaxScaler) scaler transform object
+
+        scaler:
+            - df:         (DataFrame) unscaled data
+        """
+
+        # extracting values from dataframe
+        df = df_scaled.copy()
+        data = df.to_numpy()
+        
+        # undo normaliztion
+        unscaled = scaler.inverse_transform(data)
+
+        # store unscaled values and return
+        df[:] = unscaled
+
+        return df
